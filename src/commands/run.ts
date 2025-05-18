691d2085b9e24c5055328c7f0a62035f3a702ef9
benya7 <bitup.games@gmail.com>
2025-05-18 19:30:48 +0200

    update: run command

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -1,16 +1,50 @@
+import { Libp2pCreateOptions, Peerbit } from 'peerbit';
 import type { CommandModule } from 'yargs';
+import { GlobalOptions, SiteConfig } from '../types';
+import { readConfig } from '../utils.js';
+import { Site } from '@riffcc/lens-sdk';
 
-type RunCommandArgs = {}
-
-const runCommand: CommandModule<{}, RunCommandArgs> = {
+type RunCommandArgs = {
+  relay?: boolean;
+  domains?: string []
+}
+const runCommand: CommandModule<{}, GlobalOptions & RunCommandArgs> = {
   command: 'run',
-  describe: 'Starts a long-running process that listens for SIGINT.',
-  builder: (yargs) =>
-    yargs
-    ,
+  describe: 'Starts node daemon.',
   handler: async (argv) => {
-    console.log('Process started. Waiting for SIGINT (Ctrl+C)...');
+    let siteConfig: SiteConfig | undefined;
+    try {
+      siteConfig = readConfig(argv.dir);
+    } catch (error) {
+      throw error;
+    }
 
+    let libp2pConfig: Libp2pCreateOptions | undefined = undefined;
+    if (argv.domains) {
+      libp2pConfig = {
+        addresses: {
+          announce: argv.domains
+        }
+      }
+    }
+    const client = await Peerbit.create({
+      directory: argv.dir,
+      relay: argv.relay,
+      libp2p: libp2pConfig
+    })
+
+    const site = await client.open<Site>(siteConfig.address)
+
+    console.log("Lens Node is running. Press Ctrl+C to stop.");
+    console.log("--------------------------------------------------");
+    console.log(`Node Directory: ${argv.dir}`);
+    console.log(`Peer ID: ${client.peerId.toString()}`);
+    console.log(`Node Public Key: ${client.identity.publicKey.toString()}`);
+    console.log(`Site Address: ${site.address}`);
+
+    console.log(`Listening on: ${
+      JSON.stringify(client.getMultiaddrs(), null, 2)
+    }`);
     let isShuttingDown = false;
 
     const shutdown = async (signal: string) => {
@@ -19,6 +53,8 @@ const runCommand: CommandModule<{}, RunCommandArgs> = {
 
       console.log(`\nReceived ${signal}. Shutting down gracefully...`);
 
+      await site.close();
+      await client.stop();
 
       console.log('Cleanup finished.');
 
